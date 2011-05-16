ba01b1f1ee10abc9f9a3bfec94b099aa1aee579a
Mike Pinkowish <mikedpink@gmail.com>
2011-05-15 21:44:08 -0700

    base url parse

--- src/sterling.js
+++ src/sterling.js
@@ -4,6 +4,7 @@
 
 var http = require('http');
 var mysql = require('./config/mysql.js').config;
+var url = require('url');
 
 if (process.argv.length < 3) {
   console.error("useage: node sterling.js <port>");
@@ -12,8 +13,18 @@ if (process.argv.length < 3) {
 var port = parseInt(process.argv[2], 10);
 
 http.createServer(function (req, res) {
+  var req_data = url.parse(req.url);
+  
+  // Parse the path and see if it's registered.
+  var path = req_data.pathname;
+  if (path[path.length - 1] == '/') {
+    path = path.substr(0, path.length - 1);
+  }
+
+  
+
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World<br />' + path + '\n');
 }).listen(port, "127.0.0.1");
 
 
