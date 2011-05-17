07a694b5b8e4ce3711fd26b995095dae4b0cb899
Mike Pinkowish <mikedpink@gmail.com>
2011-05-17 13:17:32 -0700

    Restructuring Sterling

--- src/sterling.js
+++ src/sterling.js
@@ -3,7 +3,6 @@
 */
 
 var http = require('http');
-var mysql = require('./config/mysql.js').config;
 var url = require('url');
 
 if (process.argv.length < 3) {
@@ -13,19 +12,27 @@ if (process.argv.length < 3) {
 var port = parseInt(process.argv[2], 10);
 
 http.createServer(function (req, res) {
-  var req_data = url.parse(req.url);
+  var req_data = url.parse(req.url, true);
   
-  // Parse the path and see if it's registered.
+  // Remove leading and trailing forward slashes.
   var path = req_data.pathname;
+  var path_len = path.length - 1;
   if (path[path.length - 1] == '/') {
-    path = path.substr(0, path.length - 1);
+    --path_len;
   }
-
-  
+  req_data.pathname = path.substr(1, path_len);
+ 
+  // First, we check if path matches a static file in ./www
+  // If not, then we'll try to see if it matches an Agent.
+  // Otherwise, we'll throw a 404.
+  findStaticFile(req_data);
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World<br />' + path + '\n');
+  res.end('Hello World\n');
 }).listen(port, "127.0.0.1");
 
+function findStaticFile(req_data) {
+
+}
 
 console.log('Sterling running at http://127.0.0.1:' + port + '/');
